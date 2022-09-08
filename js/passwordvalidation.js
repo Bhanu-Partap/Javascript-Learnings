936c246bbad8b9ad3fee774f5e5ae5b38fe9ee8b
Bhanu Partap <bhanupartap25275@gmail.com>
2022-09-08 09:40:51 +0530

    Add files via upload
    password validation js with 2 html

---
+++ js/passwordvalidation.js
@@ -0,0 +1,48 @@
+// ES5 Standard function
+
+// function fx_name(){
+//     console.log("hello its bhanu there");
+// }
+
+// ES6 Standard Function
+
+// const fx_name = function(name) { //Parameter = (name)
+//     console.log(`its ${name} here`);
+// };
+
+// fx_name("Priya"); //argument = (Priya)
+
+
+// function for password validation
+
+// let array = ['@', '#', '$', '%'];
+
+// const passwordvalidation = (password)=>{
+//     if(password.length > 8 && password.includes('@'))
+//     {
+//         console.log("Your password is strong");
+//     }
+//     else if(password.length > 8 || password.includes('@'))
+//     {
+//         console.log("your password is good");
+//     }
+//     else if(password.length < 8)
+//     {
+//         console.log("password is weak");
+//     }
+// };
+
+// passwordvalidation("bhanu@fhjah");
+
+// export default passwordvalidation();
+
+
+//----CREATING OBJECT ----//
+
+let object = {
+    name: "Bhanu",
+    age: 20,
+    location: "Ludhiana",
+};
+
+console.log(object.name);
