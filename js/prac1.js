ddbe3e90bc966301dcb6bf25d1a650bb1c734f42
Bhanu Partap <bhanupartap25275@gmail.com>
2022-09-27 22:48:21 +0530

    3

---
+++ js/prac1.js
@@ -0,0 +1,35 @@
+//q1
+
+let a = "Bhanu ";
+let b = 56;
+console.log((a+b));
+
+//q2
+console.log(typeof (a+b))
+
+//q3
+
+const a1 = {
+    name: "Bhanu",
+    Roll_no : 1906069
+}
+// a1 = {
+//     name: "Sukhjot"
+// }
+console.log(a1)
+
+//Q4
+a1['class'] = 'CSE'
+a1['name'] = "Sukhjot"
+console.log(a1)
+
+//Q5
+
+const dict = {
+    hello: "greeetings",
+    hinder: "make it difficult for (someone) to do something",
+    intended: "planned or meant"
+}
+// console.log(dict.hinder)
+// console.log(dict.hello)
+console.log(dict.intended)
